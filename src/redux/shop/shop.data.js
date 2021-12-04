dd991c31446095dd5243a0d1436a36f33c19e89a
blaaberghytten <paw@blaaberghytten.dk>
2021-12-04 10:29:45 +0100

    moved our directory and shop data into own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/redux/shop/shop.data.js
+++ src/redux/shop/shop.data.js
@@ -1,9 +1,10 @@
-const SHOP_DATA = [
-    {
+const SHOP_DATA = {
+    hats: {
         id: 1,
         title: 'Hats',
         routeName: 'hats',
-        items: [
+        items:
+        [
             {
                 id: 1,
                 name: 'Brown Brim',
@@ -60,11 +61,12 @@ const SHOP_DATA = [
             }
         ]
     },
-    {
+    sneakers: {
         id: 2,
         title: 'Sneakers',
         routeName: 'sneakers',
-        items: [
+        items:
+        [
             {
                 id: 10,
                 name: 'Adidas NMD',
@@ -115,11 +117,12 @@ const SHOP_DATA = [
             }
         ]
     },
-    {
+    jackets: {
         id: 3,
         title: 'Jackets',
         routeName: 'jackets',
-        items: [
+        items:
+        [
             {
                 id: 18,
                 name: 'Black Jean Shearling',
@@ -152,11 +155,12 @@ const SHOP_DATA = [
             }
         ]
     },
-    {
+    womens: {
         id: 4,
         title: 'Womens',
         routeName: 'womens',
-        items: [
+        items:
+        [
             {
                 id: 23,
                 name: 'Blue Tanktop',
@@ -201,11 +205,12 @@ const SHOP_DATA = [
             }
         ]
     },
-    {
+    mens: {
         id: 5,
         title: 'Mens',
         routeName: 'mens',
-        items: [
+        items:
+        [
             {
                 id: 30,
                 name: 'Camo Down Vest',
@@ -244,6 +249,6 @@ const SHOP_DATA = [
             }
         ]
     }
-];
+};
 
 export default SHOP_DATA;
